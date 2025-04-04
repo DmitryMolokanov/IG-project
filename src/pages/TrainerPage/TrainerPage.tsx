08724e7dc6a12889856b3394afc9c185a8143902
PULSAR\molokanov <molokanov@pulsarm.ru>
2025-04-04 14:46:59 +0300

    fiat(trainer): added cheking correctness answer and counter

--- src/pages/TrainerPage/TrainerPage.tsx
+++ src/pages/TrainerPage/TrainerPage.tsx
@@ -4,11 +4,15 @@ import { VerbsObjType } from '../../Types/types';
 import { verbs } from '../../constant/verbs';
 import { verbsConvertList } from '../../constant/verbsConvert';
 
+const initialInfo = { allVerbs: verbs.length, repeated: 0, correct: 0, incorrect: 0 }
+
 const TrainerPage = () => {
 
     const [currentVerb, setCurrentVerb] = useState<VerbsObjType>()
     const [remaningVerbs, setRemaningVerbs] = useState<VerbsObjType[] | undefined>(undefined)
     const [showTranslate, setShowTranslate] = useState(false)
+    const [info, setInfo] = useState(initialInfo)
+
 
     const deleteCurrentVerb = (arrVerbs: VerbsObjType[]) => {
         const result = arrVerbs.filter((item) => item.word !== currentVerb?.word)
@@ -20,15 +24,28 @@ const TrainerPage = () => {
         setCurrentVerb(arrVerbs[verbIndex])
     }
 
-    const checkVerb = (type: string) => {
-        console.log(type === currentVerb?.type)
+    const checkVerb = (type: string, e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         let newVerbsList: VerbsObjType[] = []
+        const target = e.target as HTMLElement
+
+        const returnBackground = () => setTimeout(() => target.style.background = 'royalblue', 200)
+
+        if (type === currentVerb?.type) {
+            target.style.background = 'green'
+            returnBackground()
+            setInfo({ ...info, repeated: info.repeated + 1, correct: info.correct + 1 })
+        } else {
+            target.style.background = 'red'
+            returnBackground()
+            window.navigator.vibrate(200)
+            setInfo({ ...info, repeated: info.repeated + 1, incorrect: info.incorrect + 1 })
+        }
+
         if (remaningVerbs) {
             newVerbsList = deleteCurrentVerb(remaningVerbs)
         } else {
             newVerbsList = deleteCurrentVerb(verbs)
         }
-        console.log(newVerbsList)
         setRemaningVerbs(newVerbsList)
         getRandomVerb(newVerbsList)
     }
@@ -45,7 +62,19 @@ const TrainerPage = () => {
     return (
         <div className='trainer__container'>
             <div className='trainer__info'>
-
+                <div className='trainer__info__number'>
+                    <span>{info.allVerbs}</span> / <span>{info.repeated}</span>
+                </div>
+                <div className='trainer__info__answer'>
+                    <div className='trainer__info__answer-correct'>
+                        <span className='trainer__info__answer-correct__title'>Correct:</span>
+                        <span className='trainer__info__answer-count'>{info.correct}</span>
+                    </div>
+                    <div className='trainer__info__answer-incorrect'>
+                        <span className='trainer__info__answer-incorrect__title'>Incorrect:</span>
+                        <span className='trainer__info__answer-count'>{info.incorrect}</span>
+                    </div>
+                </div>
             </div>
 
             <div
@@ -64,7 +93,13 @@ const TrainerPage = () => {
             </div>
             <div className='trainer__btn-container'>
                 {verbsConvertList.map((item) => {
-                    return <button onClick={() => checkVerb(item.type)}>{item.title}</button>
+                    return <button
+                        key={item.title}
+                        className='trainer__btn'
+                        onClick={(e) => checkVerb(item.type, e)}
+                    >
+                        {item.title}
+                    </button>
                 })}
             </div>
 
